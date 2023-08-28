dfb83fb84a6abc297d54887fb2545e50673f56ec
Kniknafs2001 <kniknafs@atu.edu>
2023-08-28 15:19:21 -0500

    updated sheets webapp

--- scripts/script.js
+++ scripts/script.js
@@ -27,19 +27,20 @@ function phoneMask() {
 }
 $('[type="tel"]').keyup(phoneMask);
 
+
 //form submit intercept
 window.addEventListener("load", function() {
-  const form = document.getElementById('contact-form');
-  form.addEventListener("submit", function(e) {
-    e.preventDefault();
-    const data = new FormData(form);
-    const action = e.target.action;
-    fetch(action, {
-      method: 'POST',
-      body: data,
-    })
-    .then(() => {
-      alert("Contact Form Submitted!");
-    })
-  });
+    const form = document.getElementById('contact-form');
+    form.addEventListener("submit", function(e) {
+        e.preventDefault();
+        const data = new FormData(form);
+        const action = e.target.action;
+        fetch(action, {
+            method: 'POST',
+            body: data,
+        })
+        .then(() => {
+            alert("Contact Form Submitted!");
+        })
+    });
 });
