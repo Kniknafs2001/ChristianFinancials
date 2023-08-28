4d6c0a8bb62beef7a18834135a3b8b60b075a69a
Kniknafs2001 <kniknafs@atu.edu>
2023-08-28 12:54:59 -0500

    Add PHP Protocol
    Added PHP Protocol file and code, updated form on index.html

---
+++ connect.php
@@ -0,0 +1,23 @@
+<?php
+    $fName = $_POST['First Name'];
+    $lName = $_POST['Last Name'];
+    $email = $_POST['Email'];
+    $phone = $_POST['Phone Number'];
+    $message = $_POST['Message'];
+  
+    //Database connection
+
+    $conn = new mysqli('localhost', 'root', '', 'test')
+
+    if($conn->connect_error) {
+        die('Connection Failed  : '.$conn->connect_error);
+    } else {
+        $stmt = $conn->prepare("insert into Contact Form(First Name, Last Name, Email, Phone Number, Message)
+        values(?, ?, ?, ?, ?)");
+        $stmt->bind_param("sssss", $fName, $lName, $email, $phone, $message);
+        $stmt->execute();
+        echo "Contact Form submitted!";
+        $stmt->close();
+        $conn->close();
+    }
+?>
