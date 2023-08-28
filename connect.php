6e358464e06c2e3ff37b66b103ba4bc5ac26b7d4
Kniknafs2001 <kniknafs@atu.edu>
2023-08-28 13:16:16 -0500

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,18 +1,18 @@
 <?php
-    $fName = $_POST['First Name'];
-    $lName = $_POST['Last Name'];
-    $email = $_POST['Email'];
-    $phone = $_POST['Phone Number'];
-    $message = $_POST['Message'];
+    $fName = $_POST['fName'];
+    $lName = $_POST['lName'];
+    $email = $_POST['email'];
+    $phone = $_POST['phone'];
+    $message = $_POST['message'];
   
     //Database connection
 
-    $conn = new mysqli('localhost', 'u637671007_CF1', 'qN3!FA&]oS6', 'u637671007_CF1_Form_Subs')
+    $conn = new mysqli('localhost', 'u637671007_CF1', 'qN3!FA&]oS6', 'u637671007_CF1_Form_Subs');
 
     if($conn->connect_error) {
         die('Connection Failed  : '.$conn->connect_error);
     } else {
-        $stmt = $conn->prepare("insert into Contact Form(First Name, Last Name, Email, Phone Number, Message)
+        $stmt = $conn->prepare("insert into Contact Form(fName, lName, email, phone, message)
         values(?, ?, ?, ?, ?)");
         $stmt->bind_param("sssss", $fName, $lName, $email, $phone, $message);
         $stmt->execute();
